62fd100f8b05699f480fdea7d2eb89a4661490f5
Dina Nevhod <Dina_Nevhod@epam.com>
2023-03-20 13:56:10 +0100

    Task15.4

---
+++ Task9.2/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9._2
+{
+    public class Calculator
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine($"{a}+{b} = {a+b}");
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine($"{a}-{b} = {a - b}");
+        }
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine($"{a}*{b} = {a * b}");
+        }
+        public void Div(int a, int b)
+        {
+            if (b==0)
+            {
+                throw new Exception("Спроба ділення на нуль");
+            }
+            Console.WriteLine($"{a}/{b} = {a / b}");
+        }
+    }
+}
---
+++ Task9.2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Task9._2.Program;
+
+namespace Task9._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Calculator calculator = new Calculator();
+            int x, y;
+            string dia;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введіть перше число: ");
+                    x = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введіть друге число: ");
+                    y = Int32.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Яку дію над числами ви хотіли б виконати?");
+                    dia = Console.ReadLine();
+
+
+                    switch (dia)
+                    {
+                        case "Add":
+                            calculator.Add(x, y);
+                            break;
+                        case "Sub":
+                            calculator.Sub(x, y);
+                            break;
+                        case "Mul":
+                            calculator.Mul(x, y);
+                            break;
+                        case "Div":
+                            calculator.Div(x, y);
+                            break;
+                        default: throw new Exception("Такої дії немає");
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Неправильно ввели число");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+        }
+    }
+}
